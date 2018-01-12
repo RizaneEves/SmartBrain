5a2f0c78b297b3885195af53b113901c8747227b
Sheel Parekh <sdparek2@illinois.edu>
2018-01-12 17:43:17 -0500

    Fix connection to back-end

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
 
 	onSubmitSignIn = () => {
 		const { email, name, password } = this.state;
-		fetch('http://localhost:3000/register', {
+		fetch('https://pacific-shelf-75158.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -20,7 +20,7 @@ class SignIn extends React.Component {
 
 	onSubmitSignIn = () => {
 		const { signInEmail, signInPassword } = this.state;
-		fetch('http://localhost:3000/signin', {
+		fetch('https://pacific-shelf-75158.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
