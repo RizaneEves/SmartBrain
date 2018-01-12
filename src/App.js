c1e9fa1f0f9fbd6a8fc2d80e9b7fb2a9a9a34a8e
Sheel Parekh <sdparek2@illinois.edu>
2018-01-12 14:56:20 -0500

    Move Image API to backend, and add form validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -13,10 +12,6 @@ import './App.css';
 
 
 // initialize with your api key. This will also work in your browser via http://browserify.org/
-
-const app = new Clarifai.App({
- apiKey: 'c9ea14d2f62c4b8099c02ee0a388fdf4'
-});
 
 
 const particlesOptions = {
@@ -87,10 +82,14 @@ class App extends Component {
   
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => {
         if(response) {
           fetch('http://localhost:3000/image', {
